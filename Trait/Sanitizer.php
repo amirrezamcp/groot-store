0b88614ca9aaf578499a40fe75ff611bde55bd9c
amirrezamcp <yt.bigpotato@gmail.com>
2024-05-19 22:21:31 +0330

    created Sanitizer

---
+++ Trait/Sanitizer.php
@@ -0,0 +1,20 @@
+<?php
+namespace Trait\Sanitizer;
+
+trait Sanitizer {
+
+    public static function sanitizeInput($input) {
+        if(is_array($input)) {
+            return array_map([self::class, 'sanitize'], $input);
+        }else{
+            return self::sanitize($input);
+        }
+    }
+
+    public static function sanitizeValue($value) {
+        $sanitizedValue = trim($value);
+        $sanitizedValue = filter_var($sanitizedValue, FILTER_SANITIZE_STRING);
+        $sanitizedValue = htmlspecialchars($sanitizedValue, ENT_QUOTES, 'UTF-8');
+        return $sanitizedValue;
+    }
+}
