fde6cc630a4dbd830a09fae3f91e977352568a89
amirrezamcp <yt.bigpotato@gmail.com>
2024-06-28 14:46:13 +0330

    configtion

--- app/core/config.php
+++ app/core/config.php
@@ -1,12 +1,16 @@
 <?php
 
 define("SITE_TITEL", "Groot Store");
+define("LS_TITEL", "login/singup");
+define("ROOT", "http://localhost/php-expert/PHP-Project/groot-store/public/");
 
 
+/**
+ * Constants Database
+ */
 define("HOST", "localhost");
 define("USERNAME", "root");
 define("PASSWORD", "");
 define("DB", "groot-store");
 
 
-// private $dsn = "mysql:host=". self::host . "; dbname=" . self::db;
