17391e6445afd51adad8375877211d8319900f12
amirrezamcp <yt.bigpotato@gmail.com>
2024-06-28 14:04:18 +0330

    created config

---
+++ app/core/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define("SITE_TITEL", "Groot Store");
+
+
+define("HOST", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+define("DB", "groot-store");
+
+
+// private $dsn = "mysql:host=". self::host . "; dbname=" . self::db;
