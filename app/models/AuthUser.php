9ca23419866398a8ffc1cb9180b4216780855848
amirrezamcp <yt.bigpotato@gmail.com>
2024-06-30 12:00:30 +0330

    check password and hash password and insert phone number

--- app/models/AuthUser.php
+++ app/models/AuthUser.php
@@ -1,14 +1,50 @@
 <?php
+
 use Helpers\Sanitizer;
+use Helpers\Semej;
 
 class AuthUser extends Database {
     public function register($csrf_token, $formData) {
+        
         $csrf_token = Sanitizer::sanitizeInput($csrf_token);
-        $formData   = Sanitizer::sanitizeInput($formData);
+        $formData = Sanitizer::sanitizeInput($formData);
 
+        // check password
+        if($formData['password'] != $formData['confirm_password']) {
+            Semej::set('error','rejection password', 'لطفا رمز ها را یکسان وارد کنید');
+            header('location:login-singup.php');
+        }
+
+        // hash password
+        $password_hash = password_hash($formData['password'], PASSWORD_BCRYPT);
+
+        $checkValidatePhoneNumber = $this->validatePhoneNumber($formData['phoneNumber']);
+        if(!$checkValidatePhoneNumber) {
+            Semej::set('error','rejection PhoneNumber', 'شماره خودتو هم بلد نیسی ؟این تاسف بار است!');
+            header('location:login-singup.php');
+        }
+        // insert phone number
         $sql = "INSERT INTO users (userName, phone_number, password) VALUES (:username, :phone_number, :password)";
         $stmt = $this->connection->prepare($sql);
-        $stmt->execute([':username' => $formData['username'], ':phone_number' => $formData['phoneNumber'], ':password' => $formData['password']]);
+        $stmt->execute([':username' => $formData['username'], ':phone_number' => $formData['phoneNumber'], ':password' => $password_hash]);
         return $stmt->rowCount();
     }
+    
+    function validatePhoneNumber($phoneNumber) {
+        $phoneNumber = Sanitizer::sanitizeInput($phoneNumber);
+        $phoneNumber = preg_replace('/[^0-9]/', '', $phoneNumber);
+        
+        if (!is_string($phoneNumber)) {
+            return false;
+        }
+        if (strlen($phoneNumber) != 11) {
+            return false;
+        }
+        if (substr($phoneNumber, 0, 2) != '09') {
+            return false;
+        }
+        return true;
+    }
+    
+    
 }
