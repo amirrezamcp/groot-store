570bdc0026d4b0ecc4499354e2fd6414e124d8ec
“Abolfazl <abolfazl.mangaliyan.sh@gmail.com>
2024-07-02 01:14:48 +0330

    now you can see password

--- public/assets/js/login-singup/login-singup.js
+++ public/assets/js/login-singup/login-singup.js
@@ -1,8 +1,28 @@
+
 let $ = document
 let loginForm = $.querySelector('.login-Form')
 let singUpForm = $.querySelector('.singUp-Form')
 let singUpClick = $.querySelector('.singUp-Page')
 let loginClick = $.querySelector('.login-Page')
+let singUpBtn = $.querySelector('.singUp-Btn')
+let eyeIcon = $.querySelectorAll('.eye-Icon')
+let passwordInput = $.querySelectorAll('.password-Input')
+
+let eyeIconClick = 0
+
+eyeIcon.forEach(el => {
+    el.addEventListener('click', function() {
+        eyeIconClick ++ 
+        if (eyeIconClick % 2 == 0)
+            passwordInput.forEach(e => {
+                e.setAttribute('type', 'password')
+            });
+        else
+        passwordInput.forEach(e => {
+            e.setAttribute('type', 'text')
+        });
+    })   
+});
 
 loginClick.addEventListener('click', function() {
     loginForm.style.display = 'flex'
@@ -12,3 +32,8 @@ singUpClick.addEventListener('click', function() {
     loginForm.style.display = 'none'
     singUpForm.style.display = 'flex'
 })
+singUpBtn.addEventListener('click', function() {
+    singUpForm.submit()
+    
+})
+
